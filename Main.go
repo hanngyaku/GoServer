5f44c4f3e2d06182e6db6a242f08673bb1e2142e
chenganggui <chenganggui@foxmail.com>
2024-04-07 16:01:14 +0800

    添加测试json文件

--- Main.go
+++ Main.go
@@ -1,36 +1,89 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
+type Company struct {
+	Name string
+	Path string
+}
+
 func main() {
-	// 设置文件服务器的根目录
-	rootDir := "E:/Test/" // 替换为你的本地文件路径
+	http.FileServer(http.Dir("./")) // 替换为你的静态文件路径
 
-	// 创建一个文件服务器实例
-	fs := http.FileServer(http.Dir(rootDir))
-
-	// 定义一个处理函数，用于处理请求并将请求重定向到文件服务器
+	// 处理根路径请求，打开 index.html 文件
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// 获取请求的文件路径
-		requestedFile := filepath.Join(rootDir, r.URL.Path)
-		fmt.Println(requestedFile)
-		// 检查请求的文件是否存在
-		if _, err := os.Stat(requestedFile); err == nil {
-			// 请求的文件存在，将请求重定向到文件服务器
-			fs.ServeHTTP(w, r)
-		} else {
-			// 请求的文件不存在，返回404错误
-			http.NotFound(w, r)
-		}
+		http.ServeFile(w, r, "./index.html") // 替换为你的 index.html 文件路径
 	})
+
+	//// 将文件服务器处理器注册到路由
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("e:/"))))
+	//http.Handle("/image/", http.StripPrefix("/web/", http.FileServer(http.Dir("./images"))))
+	//http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
+
+	// http.HandleFunc("/QueCompany", QueCompanyHandle)
+	http.HandleFunc("/QueCompany", QueCompanyHandle2)
 
 	// 启动HTTP服务器并监听指定端口
 	port := "8080" // 替换为你希望使用的端口号
 	fmt.Printf("Server is running on port %s...\n", port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+// 返回所有公司名称，通过json
+func QueCompanyHandle(w http.ResponseWriter, r *http.Request) {
+	// 设置响应头
+	// w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// 如果是预检请求（OPTIONS），则直接返回成功状态码
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	company := Company{
+		Name: "测试",
+		Path: "测试",
+	}
+	jsonData, err := json.Marshal(company)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println(jsonData)
+	// 将 JSON 数据写入响应体
+	w.Write(jsonData)
+}
+
+// 返回所有公司名称，通过json
+func QueCompanyHandle2(w http.ResponseWriter, r *http.Request) {
+	// 设置响应头
+	// w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// 如果是预检请求（OPTIONS），则直接返回成功状态码
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	data, err := os.ReadFile("./test.json") // 替换为你的 JSON 文件路径
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// 设置响应头
+	w.Header().Set("Content-Type", "application/json")
+
+	// 返回 JSON 数据
+	w.Write(data)
+
 }
